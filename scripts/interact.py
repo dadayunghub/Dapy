e453606123ac7930452319dd37c77b3a0e04c69e
dadayunghub <davidyung450@gmail.com>
2026-02-08 11:40:25 +0100

    update interact.py

--- scripts/interact.py
+++ scripts/interact.py
@@ -838,7 +838,7 @@ def to_bytes32(val):
 def mint_tokens(wallet_id, to_address, amount, CIRCLE_URL, headers):
     mint_payload = {
         "idempotencyKey": str(uuid.uuid4()),
-        "walletId": "64ca341e-31dd-5fe0-a43b-64887f28e1c6",
+        "walletId": "57732b92-a9b5-5786-9d93-ca45d6744b06",
         "contractAddress": TOKEN_ADDRESS,
         "abiFunctionSignature": "mintTo(address,uint256)",
         "abiParameters": [to_address, str(amount)],
