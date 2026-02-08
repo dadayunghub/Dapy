91ea0c160f25031b16a968e421766c13c386daaa
dadayunghub <davidyung450@gmail.com>
2026-02-08 15:45:39 +0100

    update interact.py

--- scripts/interact.py
+++ scripts/interact.py
@@ -921,7 +921,7 @@ def transferpermit(args):
             transfer_args = SimpleNamespace(
                 amount=mint_amount,
                 to=sender_address)
-                transferdev(transfer_args)
+            transferdev(transfer_args)
                 time.sleep(20)
 
         # -------- SIGN PERMIT OFF-CHAIN --------
