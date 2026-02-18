123c2d4a81688ce5290775063bf85e33e5e37716
dadayunghub <davidyung450@gmail.com>
2026-02-18 08:10:07 +0100

    update bridge.ts

--- scripts/bridge.ts
+++ scripts/bridge.ts
@@ -50,8 +50,8 @@ const bridgeUSDC = async () => {
       entitySecret: process.env.CIRCLE_ENTITY_SECRET!,
     });
     
-    const min = 0.01;
-const max = 0.09;
+    const min = 1.0;
+const max = 9.0;
 // Generate and fix to 2 decimals, then convert back to a number
 const randomAmount: number = Number((Math.random() * (max - min) + min).toFixed(2));
 
